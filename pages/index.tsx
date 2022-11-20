2c718e82ebaca9c30a1dff5645353c039e41c460
Gonzalo Daniel Aguilar <aguilar.daniel.gonzalo@gmail.com>
2022-11-20 17:38:46 -0300

    Ad funcion Fetch Collection.

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,47 @@ const Home = () => {
   const [collection, setCollectionAddress] = useState("");
   const [NFTs, setNFTs] = useState([])
 
+  const fetchNFTs = async () => {
+    let nfts;
+    console.log("fetching nfts");
+    const api_key = "A8A1Oo_UTB9IN5oNHfAc2tAxdR4UVwfM"
+    const baseURL = `https://eth-mainnet.g.alchemy.com/v2/${api_key}/getNFTs/`;
+    var requestOptions = {
+      method: 'GET'
+    };
+
+    if (!collection.length) {
+
+      const fetchURL = `${baseURL}?owner=${wallet}`;
+
+      nfts = await fetch(fetchURL, requestOptions).then(data => data.json())
+    } else {
+      console.log("fetching nfts for collection owned by address")
+      const fetchURL = `${baseURL}?owner=${wallet}&contractAddresses%5B%5D=${collection}`;
+      nfts = await fetch(fetchURL, requestOptions).then(data => data.json())
+    }
+
+    if (nfts) {
+      console.log("nfts:", nfts)
+      setNFTs(nfts.ownedNfts)
+    }
+  }
+
+  const fetchNFTsForCollection = async () => {
+    if (collection.length) {
+      var requestOptions = {
+        method: 'GET'
+      };
+      const api_key = "A8A1Oo_UTB9IN5oNHfAc2tAxdR4UVwfM"
+      const baseURL = `https://eth-mainnet.g.alchemy.com/v2/${api_key}/getNFTsForCollection/`;
+      const fetchURL = `${baseURL}?contractAddress=${collection}&withMetadata=${"true"}`;
+      const nfts = await fetch(fetchURL, requestOptions).then(data => data.json())
+      if (nfts) {
+        console.log("NFTs in collection:", nfts)
+        setNFTs(nfts.nfts)
+      }
+    }
+  }
   return (
     <div className="flex flex-col items-center justify-center py-8 gap-y-3">
       <div className="flex flex-col w-full justify-center items-center gap-y-2">
